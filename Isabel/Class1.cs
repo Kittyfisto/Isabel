9d78b572979ca96674745f98635599804cfafe95
Kittyfisto <kittyfisto87@gmail.com>
2017-08-28 20:20:55 +0200

    Add project files.

---
+++ Isabel/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isabel
+{
+    public class Class1
+    {
+    }
+}
